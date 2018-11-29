daab1a2d3c898b02442cb3f0b394b052641a298d
Will <30760648+WillDera@users.noreply.github.com>
2018-11-30 00:23:38 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var shellCacheName = "pwa-todo-v1";
+var cacheName = 'todo-pwa';
 var filesToCache = [
   "./",
   "./index.html",
@@ -7,3 +7,37 @@ var filesToCache = [
   "https://cdn.jsdelivr.net/npm/vue",
   "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css"
 ];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
